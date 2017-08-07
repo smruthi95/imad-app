a95657e166bb45b854a8eeadbb36a1f14b8eba85
smruthi95 <30722132+smruthi95@users.noreply.github.com>
2017-08-07 14:58:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ element.innerHTML='New Value';
 
 //move image 
 var img=document.getElementById('madi');
-var marginleft=0;
+var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
