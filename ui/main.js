2db6152ec0aca8860c7f53ae61a643727f7e4f77
smruthi95 <30722132+smruthi95@users.noreply.github.com>
2017-08-07 14:55:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,14 @@ element.innerHTML='New Value';
 
 //move image 
 var img=document.getElementById('madi');
+var marginleft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
-  img.style.marginLeft='100px';
+    var interval=setInterval(moveLeft,100);
+  img.style.marginRight='100px';
+  
   
 };
