ff2614deb28c440b03db9952105b6b0fc7a119c9
smruthi95 <30722132+smruthi95@users.noreply.github.com>
2017-08-22 13:05:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,12 +78,10 @@ app.get('/articles/:articleName',function(req,res){
       }
       else 
       {
-          if (result.rows.length === 0){
-          res.status(404).send('Article not found');
-          }else{
+          
               var articleData=result.rows[0];
                res.send(createTemplate(articles[articleName]));
-          }
+          
       }
    });
 });
