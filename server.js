187d630ee783a59a25efd2997692a77c60d7c0b6
smruthi95 <30722132+smruthi95@users.noreply.github.com>
2017-08-22 13:34:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,8 +81,7 @@ app.get('/articles/:articleName',function(req,res){
       {
           if (result.rows.length === 0)
           {
-              var articleData=result.rows[0];
-              res.send(createTemplate(articleData));
+              res.status(404).send('Article not found');
           }
           else
           {
