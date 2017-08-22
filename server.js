bc6cc354febefe0fff67d149b9b3584808b0f0e4
smruthi95 <30722132+smruthi95@users.noreply.github.com>
2017-08-22 13:30:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/submit-name',function(req,res){// URL:/submit-name?name=xxxxx
     res.send(JSON.stringify(names));
 });
 
-app.get('/articles/:articleName',function(req,res){
+app.get('/articles/articleName',function(req,res){
    //articleName==article-one
    
    var articleName=req.params.articleName;
