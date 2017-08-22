921ada8c3eb1716478a039a15181b8340125748e
smruthi95 <30722132+smruthi95@users.noreply.github.com>
2017-08-22 13:12:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/articles/:articleName',function(req,res){
           }
           else
           {
-              var articleData=result.rows[1];
+              var articleData=result.rows[0];
                res.send(createTemplate(articles[articleName]));
           }
       }
