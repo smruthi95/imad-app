7da0763409ccef096d198770c9faa85e2ac6faed
smruthi95 <30722132+smruthi95@users.noreply.github.com>
2017-08-22 13:29:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,15 +79,15 @@ app.get('/articles/:articleName',function(req,res){
       }
       else 
       {
-          //if (result.rows.length === 0)
-          //{
-          //res.status(404).send('Article not found');
-          //}
-          //else
-          //{
+          if (result.rows.length === 0)
+          {
+              res.status(404).send('Article not found');
+          }
+          else
+          {
               var articleData=result.rows[0];
-               res.send(createTemplate(articleData));
-          //}
+              res.send(createTemplate(articleData));
+          }
       }
    });
 });
