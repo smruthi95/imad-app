b2f693f55f050df951455563a28184c4f216e59d
smruthi95 <30722132+smruthi95@users.noreply.github.com>
2017-08-22 11:44:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
 
     user:'giraffesmruthi',
     database: 'giraffesmruthi',
-    host: 'db.imad.hasura-app.io',
+    host: 'db.imad.hasura.io',
     port: '5432',
     password:process.env.DB_PASSWORD
     
