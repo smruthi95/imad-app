3d0e8c00f180006030b930812910a1c23deaf525
smruthi95 <30722132+smruthi95@users.noreply.github.com>
2017-08-22 16:45:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,35 +1,9 @@
-//Counter code
-var button=document.getElementById('counter');
 
-button.onclick=function(){
-  //Create a request object 
-  var request=new XMLHttpRequest();
-  
-  //capture the response and store it ioon a variable
-  request.onreadystatechange = function(){
-     if (request.readyState === XMLHttpRequest.DONE){
-         //take some action
-         if (request.status === 200){
-          var counter=request.responseText;
-          var span=document.getElementById('count');
-          span.innerHTML=counter.toString();
-         }
-     }
-     //not done yet
-  };
-  
-  //make the request
-  request.open('GET','http://giraffesmruthi.imad.hasura-app.io/counter',true);
-  request.send('null');
- 
-  
-};
+//submit username/password to login
 
-//submit name
-
-var submit=document.getElementById('Submit_btn');
+var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-     //Create a request object 
+     //Create a request object
   var request=new XMLHttpRequest();
   
   //capture the response and store it ioon a variable
@@ -37,24 +11,25 @@ submit.onclick=function(){
      if (request.readyState === XMLHttpRequest.DONE){
          //take some action
          if (request.status === 200){
-    //make a request to server and snend the name
-    //capture a list of names and render it as a list
-    var names=request.responseText;
-    names=JSON.parse(names);
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
-}
+             console.log('User logges in');
+             alert('Logged in successfully');
+         }else if(request.status===403){
+             alert('Username/password incorrect');
+         }else if(request.status===500){
+             alert('Something went wrong on the server');
+             
+         }
+    
      }
      //not done yet
   };
   
   //make the request
-  var nameInput=document.getElementById('name');
-  var name=nameInput.value;
-  request.open('GET','http://giraffesmruthi.imad.hasura-app.io/submit-name?name='+name,true);
-  request.send('null');
+  var username=document.getElementById('username').value;
+  var password=document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
+  request.open('POST','http://giraffesmruthi.imad.hasura-app.io/login',true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({username:username,password:password}));
 };
